728caef8a2763cc20d7c95de87523f42e215839a
Subhradeep Samantray <subhrd@iitk.ac.in>
2020-02-28 22:41:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+
+numberOfDrums = document.querySelectorAll(".drum").length;
+
+
+for (var i = 0; i < numberOfDrums; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var buttoninnerHTML = this.innerHTML;
+    MakeSound(buttoninnerHTML);
+    SoundAnimation(buttoninnerHTML);
+  });
+
+  document.addEventListener("keypress", function(event){
+    MakeSound(event.key);
+    SoundAnimation(event.key);
+  })
+
+    function MakeSound(key){
+      switch (key){
+        case "w":
+          var tom1 = new Audio('sounds/tom-1.mp3');
+          tom1.play();
+          break;
+        case "a":
+          var tom2 = new Audio('sounds/tom-2.mp3');
+          tom2.play();
+          break;
+        case "s":
+          var tom3 = new Audio('sounds/tom-3.mp3');
+          tom3.play();
+          break;
+        case "d":
+          var tom4 = new Audio('sounds/tom-4.mp3');
+          tom4.play();
+          break;
+        case "j":
+         var snare = new Audio('sounds/snare.mp3');
+         snare.play();
+         break;
+        case "k":
+         var kick = new Audio('sounds/kick-bass.mp3');
+         kick.play();
+         break;
+        case "l":
+         var crash = new Audio('sounds/crash.mp3');
+         crash.play();
+         break;
+
+
+
+
+        default:
+      }
+    }
+
+    function SoundAnimation(currentkey){
+      var Activebutton = document.querySelector("." + currentkey);
+      Activebutton.classList.add("pressed");
+      setTimeout(function(){
+        Activebutton.classList.remove("pressed");
+      }, 100);
+
+    }
+
+}
